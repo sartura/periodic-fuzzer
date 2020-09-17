c946e8aac162317fcfd84e2898928c02d693ca22
Luka Paulic <luka.paulic0@gmail.com>
2020-09-17 15:31:03 +0200

    main: add dummy main

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+import logging
+import json
+import git
+
+import server.PeriodicFuzzer as pf
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    logging.log(logging.INFO, "not implemented")
