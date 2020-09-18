4503df1a8ae4a55c5d6cdeb708e9d96286af02d1
Luka Paulic <luka.paulic0@gmail.com>
2020-09-18 17:58:47 +0200

    main: update main file

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import sys
 
 import logging
-import json
-import git
 
+import config.ConnfigPeriodicFuzzer as cpf
 import server.PeriodicFuzzer as pf
+
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    logging.log(logging.INFO, "not implemented")
+
+    if len(sys.argv) != 2:
+        logging.log(logging.ERROR, f'usage: {sys.argv[0]} JSON_config_file')
+        exit(1)
+
+    try:
+        configPeriodicFuzzer = cpf.ConfigPeriodicFuzzer()
+        configPeriodicFuzzer.parseJSON(sys.argv[1])
+        configuration = configPeriodicFuzzer.getConfiguration()
+
+        periodicFuzzer = pf.PeriodicFuzzer(**configuration)
+        periodicFuzzer.start()
+
+    except cpf.ConfigPeriodicFuzzerError as config_error:
+        logging.log(logging.ERROR, f'main error: {config_error}')
+    except pf.PeriodiFuzzerError as periodic_error:
+        logging.log(logging.ERROR, f'main error: {periodic_error}')
